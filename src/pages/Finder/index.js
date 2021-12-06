10e85689fad3fa52950e4ff46ed47fdde6e099eb
martiplaceres <martinaplaceres@hotmail.com>
2021-12-06 10:04:13 +0100

    card design

--- src/pages/Finder/index.js
+++ src/pages/Finder/index.js
@@ -1,7 +1,145 @@
-export default function Finder() {
+import React, { useState, useEffect, useMemo, useRef } from "react";
+import axios from "axios";
+// import TinderCard from '../react-tinder-card/index'
+import TinderCard from "react-tinder-card";
+import "../../Card.css";
+
+function Advanced() {
+  const [lastDirection, setLastDirection] = useState();
+  const [allPets, setAllPets] = useState([]);
+
+  const [currentIndex, setCurrentIndex] = useState(
+    Object.keys(allPets).length - 1
+  );
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/pets`);
+        console.log("All pets:", response.data);
+        setAllPets(response.data.pets);
+      } catch (e) {
+        console.log("error:", e);
+      }
+    })();
+  }, []);
+
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(allPets.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const canGoBack = currentIndex < Object.keys(allPets).length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  // set last direction and decrease current index
+  const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < allPets.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current.restoreCard();
+  };
+
   return (
-    <div>
-      <h1>Finder</h1>
+    <div className="finderPage">
+      <div></div>
+      <link
+        href="https://fonts.googleapis.com/css?family=Damion&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
+        rel="stylesheet"
+      />
+      <h1 className="titletext">Find your Pet/Mate!</h1>
+      <div className="cardContainer">
+        {allPets
+          ? allPets.map((character, index) => (
+              <TinderCard
+                ref={childRefs[index]}
+                className="swipe"
+                key={character.name}
+                onSwipe={(dir) => swiped(dir, character.name, index)}
+                onCardLeftScreen={() => outOfFrame(character.name, index)}
+              >
+                <div
+                  style={
+                    {
+                      // backgroundImage: "url(" + character.photos[1] + ")",
+                    }
+                  }
+                  className="card"
+                >
+                  {console.log("TIene fOto?", character.photos[1])}
+                  <h3 className="h3">{character.name}</h3>
+                </div>
+              </TinderCard>
+            ))
+          : null}
+      </div>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("left")}
+        >
+          Swipe left!
+        </button>
+        <button
+          style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+          onClick={() => goBack()}
+        >
+          Undo swipe!
+        </button>
+        <button
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("right")}
+        >
+          Swipe right!
+        </button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className="infoText">
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className="infoText">
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
     </div>
   );
 }
+
+export default Advanced;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,132 @@
+import "../../App.css";
+import { Card, CardGroup, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <div className="body"></div>
-      <div className="testimonies"></div>
+      <div className="backgroundImage"></div>
+      <div className="cardPage" style={{ padding: "150px" }}>
+        <h1
+          style={{
+            fontSize: 110,
+            zIndex: 1,
+            display: "flex",
+            justifyContent: "center",
+            verticalAlign: "center",
+            textShadow: "0px 0px 5px white",
+            color: "white",
+          }}
+        >
+          Tinpet
+        </h1>
+        <h2
+          style={{
+            fontSize: 30,
+            marginTop: -20,
+            display: "flex",
+            justifyContent: "center",
+            color: "white",
+          }}
+        >
+          Matching pets since 1992.
+        </h2>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            verticalAlign: "center",
+          }}
+        >
+          <Link to="/signup">
+            <Button variant="success">Sign up</Button>{" "}
+          </Link>
+        </div>
+      </div>
+      <div
+        style={{
+          padding: "40px",
+
+          backgroundColor: "hsla(0, 0%, 0%, 0.3)",
+        }}
+      >
+        <CardGroup>
+          <Card style={{ borderStyle: "solid" }}>
+            <Card.Img
+              style={{
+                borderStyle: "solid",
+
+                width: "100%",
+                borderRadius: "5px",
+                height: "280px",
+                objectFit: "cover",
+              }}
+              variant="top"
+              src="https://trudog.com/wp-content/uploads/2016/01/shutterstock_245908141-1.jpg"
+            />
+            <Card.Body style={{ borderStyle: "solid", borderRadius: "5px" }}>
+              <Card.Title style={{ color: "green" }}>Toby & Katty</Card.Title>
+              <Card.Text>
+                " We met through the app. Since the first date we knew we were
+                meant for each other. Now we have 8 beautiful children and we
+                are all happy dogs. Thank you Tinpet. You've changed our
+                lifes!!!"
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer style={{ borderStyle: "solid" }}>
+              <small className="text-muted">5 years together.</small>
+            </Card.Footer>
+          </Card>
+          <Card style={{ borderStyle: "solid" }}>
+            <Card.Img
+              style={{
+                borderStyle: "solid",
+                width: "100%",
+                height: "280px",
+                borderRadius: "5px",
+                objectFit: "cover",
+              }}
+              variant="top"
+              src="https://cdn3.vectorstock.com/i/1000x1000/53/92/loyalty-card-icon-outline-style-vector-27435392.jpg"
+            />
+            <Card.Body style={{ borderStyle: "solid" }}>
+              <Card.Title style={{ color: "green" }}>
+                Bonnie and Clyde
+              </Card.Title>
+              <Card.Text>
+                "We were looking for something casual but we ended up falling in
+                love with each other. Now we explore together the suburbs of the
+                city!"{" "}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer style={{ borderStyle: "solid" }}>
+              <small className="text-muted">1.5 years together.</small>
+            </Card.Footer>
+          </Card>
+          <Card style={{ borderStyle: "solid" }}>
+            <Card.Img
+              style={{
+                borderStyle: "solid",
+                borderRadius: "5px",
+                width: "100%",
+                height: "280px",
+                objectFit: "cover",
+              }}
+              variant="top"
+              src="https://images.photowall.com/products/57204/dogs-kissing.jpg?h=699&q=85"
+            />
+            <Card.Body style={{ borderStyle: "solid" }}>
+              <Card.Title style={{ color: "green" }}>Puff & Plaff</Card.Title>
+              <Card.Text>
+                "It was a love from the first sight. Exploring nature together
+                is the best thing! Thank you Tinpet!"
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer style={{ borderStyle: "solid" }}>
+              <small className="text-muted">1 month together.</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+      </div>
     </div>
   );
 }
