2e79de01181d956671b46c7e65c22278fceb01b1
muftic <marijan.muftic@gmail.com>
2021-12-07 14:09:40 +0100

    Added petProfile edits, add picture

--- src/pages/PetProfile/index.js
+++ src/pages/PetProfile/index.js
@@ -1,7 +1,63 @@
 import React from "react";
 import { useParams } from "react-router";
+import { useState } from "react";
+import { Image } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+import { apiUrl } from "../../config/constants";
 export default function PetProfile() {
   const id = Number(useParams().petId);
+  const [imageData, setImageData] = useState();
+  const sendImage = async () => {
+    try {
+      const response = await axios.post(`${apiUrl}/photos/`, {
+        imageUrl: imageData,
+        petId: id,
+      });
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        //dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        console.log(error.message);
+        //dispatch(setMessage("danger", true, error.message));
+      }
+    }
+  };
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    //first parameter is always upload_preset, second is the name of the preset
+    data.append("upload_preset", "c5h84nci");
+
+    //post request to Cloudinary, remember to change to your own link
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/muftic/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const file = await res.json();
+    console.log("file", file); //check if you are getting the url back
+    setImageData(file.url); //put the url in local state, next step you can send it to the backend
+    /*     try {
+      const response = await axios.post(`${apiUrl}/photos/`, {
+        imageUrl: imageData,
+        petId: id,
+      });
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        //dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        console.log(error.message);
+        //dispatch(setMessage("danger", true, error.message));
+      }
+    } */
+  };
   return (
     <div>
       {" "}
@@ -18,8 +74,29 @@ export default function PetProfile() {
         <p>
           species <input type="text"></input>
         </p>
-
         <button>Change</button>
+        <div
+          style={{
+            display: "inline-block",
+            border: "1px solid black",
+            width: "50%",
+          }}
+        >
+          <input type="file" onChange={uploadImage} />
+          <div style={{ textAlign: "center" }}>
+            <Image
+              id="thisOne"
+              crossOrigin="anonymous"
+              style={{ height: "50px", width: "50px" }}
+              src={
+                imageData
+                  ? imageData
+                  : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+              }
+            />
+          </div>
+          // <Button onClick={() => sendImage()}>SUBMIT</Button>
+        </div>{" "}
       </div>
     </div>
   );
